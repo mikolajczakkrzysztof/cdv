307fad08234ad3bc41c178b9d1494a9b35af179f
Krzysztof Mikołajczak <kmikolajczak@edu.cdv.pl>
2019-05-20 12:30:02 +0200

    commit

--- programowanie_strukturalne/10_1.py
+++ programowanie_strukturalne/10_1.py
@@ -16,5 +16,26 @@ for i in range(a,b-1,-1):
     print(i,end=' ')
 print()
 
+print("\n\n")
+
+print("Podaj wysokosc choinki: ")
+x=int(input())
+print("Podaj szerokosc choinki: ")
+y=int(input())
+
+for i in range(0,x):
+    for j in range(1,y+1):
+        print(j * '*')
+print()
+print("\n\n")
+
+ilosc=int(input("ilosc wierszy: "))
+znak=input("Znak: ")
+
+for i in range(1,ilosc+1):
+    print (znak *i)
+    i=i+1
+
+
 
 
